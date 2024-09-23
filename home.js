66a17409ecfde7f8b618ca77bcc5abf48f314ede
pran-to <prantochakrobortty24@gmail.com>
2024-09-23 21:59:44 +0600

    added Modal

--- home.js
+++ home.js
@@ -1,18 +1,16 @@
-// donation and history toggle 
-document.getElementById('donation-btn').addEventListener('click', ()=>{
+// donation and history toggle
+document.getElementById("donation-btn").addEventListener("click", () => {
+  doToggle("donation-btn", "history-btn", "cards", "history-section");
+});
 
-    doToggle('donation-btn', 'history-btn' , 'cards' , 'history-section');
-})
+document.getElementById("history-btn").addEventListener("click", () => {
+  doToggle("history-btn", "donation-btn", "history-section", "cards");
+});
 
-document.getElementById('history-btn').addEventListener('click', ()=>{  
-        doToggle('history-btn', 'donation-btn' , 'history-section' , 'cards');
-})
-
-
-// Donation functionality 
-// card 1 
-donateFun('title-1','donation-btn1','donation-input-1','donationAmount-1')
+// Donation functionality
+// card 1
+donateFun("title-1", "donation-btn1", "donation-input-1", "donationAmount-1");
 //  card 2
-donateFun('title-2','donation-btn2','donation-input-2','donationAmount-2')
+donateFun("title-2", "donation-btn2", "donation-input-2", "donationAmount-2");
 // card 3
-donateFun('title-3','donation-btn3','donation-input-3','donationAmount-3')
+donateFun("title-3", "donation-btn3", "donation-input-3", "donationAmount-3");
--- utilities.js
+++ utilities.js
@@ -8,75 +8,83 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// toggle button function
 
-// toggle button function 
-
-
-function doToggle(a , b , c , d){
- document.getElementById(a).classList.add('bg-blue-500');
- document.getElementById(a).classList.remove('bg-gray-500');
- document.getElementById(b).classList.add('bg-gray-500');
- document.getElementById(b).classList.remove('bg-blue-500');
- document.getElementById(c).classList.remove('hidden');
- document.getElementById(d).classList.add('hidden');
+function doToggle(a, b, c, d) {
+  document.getElementById(a).classList.add("bg-blue-500");
+  document.getElementById(a).classList.remove("bg-gray-500");
+  document.getElementById(b).classList.add("bg-gray-500");
+  document.getElementById(b).classList.remove("bg-blue-500");
+  document.getElementById(c).classList.remove("hidden");
+  document.getElementById(d).classList.add("hidden");
 }
 
-// donation all main function 
-function donateFun(title ,donationBtn,donationIn,donationAm){
-  document.getElementById(donationBtn).addEventListener('click',(e)=>{
-      e.preventDefault();
-      
-              if (isNaN(inputFiled(donationIn)) || inputFiled(donationIn) <= 0) {
-                  alert("Invalid donation amount.");
-                  return;
-              }
-             else if (inputFiled(donationIn) > textFiled('main-balance')) {
-                  alert("Insufficient balance.");
-                  return;
-              }
-              else {
-                  const donationTotal1 = textFiled(donationAm) + inputFiled(donationIn)  ;
-                  const mainBalance = textFiled('main-balance') - inputFiled(donationIn) ;
-                  document.getElementById(donationAm).innerText = donationTotal1 ;
-                  document.getElementById('main-balance').innerText = mainBalance ;
-                  // history update section 
-                   historyUpdate (title,donationIn);
+// donation all main function
 
-                   document.getElementById(donationIn).value = '';
-  
-                  //  modal part 
-              }
-  
-  
-       
-  })
+function donateFun(title, donationBtn, donationIn, donationAm) {
+  const modal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
+  const closeModal = document.getElementById("close-modal");
+  document.getElementById(donationBtn).addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (isNaN(inputFiled(donationIn)) || inputFiled(donationIn) <= 0) {
+      alert("Invalid donation amount.");
+
+      return;
+    } else if (inputFiled(donationIn) > textFiled("main-balance")) {
+      alert("Insufficient balance.");
+
+      return;
+    } else {
+      const donationTotal1 = textFiled(donationAm) + inputFiled(donationIn);
+      const mainBalance = textFiled("main-balance") - inputFiled(donationIn);
+      document.getElementById(donationAm).innerText = donationTotal1;
+      document.getElementById("main-balance").innerText = mainBalance;
+      // history update section
+      historyUpdate(title, donationIn);
+
+      //  modal part
+      // Show modal
+      modalMessage.textContent = `
+                       Your Donation Amount : ${inputFiled(donationIn)} Taka`;
+      modal.classList.remove("hidden");
+
+      document.getElementById(donationIn).value = "";
+    }
+  });
+  // Close modal
+  closeModal.addEventListener("click", function () {
+    modal.classList.add("hidden");
+  });
 }
 
-// input filed get value function  
+// input filed get value function
 
-function inputFiled (id) {
-  const inputFiled = document.getElementById(id).value ;
-    const input = parseFloat(inputFiled);
-    return input ;
+function inputFiled(id) {
+  const inputFiled = document.getElementById(id).value;
+  const input = parseFloat(inputFiled);
+  return input;
 }
-// text filed get value function  
+// text filed get value function
 
-function textFiled (id) {
-  const textFiled = document.getElementById(id).innerText ;
-    const text = parseFloat(textFiled);
-    return text ;
+function textFiled(id) {
+  const textFiled = document.getElementById(id).innerText;
+  const text = parseFloat(textFiled);
+  return text;
 }
 
-// create element and history update function 
-function historyUpdate (titles,donationInput){
-  let history = document.getElementById('history-section');
-  const title = document.getElementById(titles).innerText ;
-  const newDiv = document.createElement('div');
-    newDiv.innerHTML = `
-    <div class=" rounded-md drop-shadow-md space-y-7 bg-white p-7 border "> <p class = "font-bold "> ${ inputFiled(donationInput)} Taka is  ${title} </p> 
+// create element and history update function
+function historyUpdate(titles, donationInput) {
+  let history = document.getElementById("history-section");
+  const title = document.getElementById(titles).innerText;
+  const newDiv = document.createElement("div");
+  newDiv.innerHTML = `
+    <div class=" rounded-md drop-shadow-md space-y-7 bg-white p-7 border "> <p class = "font-bold "> ${inputFiled(
+      donationInput
+    )} Taka is  ${title} </p> 
     <p> ${new Date().toLocaleString()} </p>
     </div>
-    `
-    history.appendChild(newDiv);
-
- }
+    `;
+  history.appendChild(newDiv);
+}
